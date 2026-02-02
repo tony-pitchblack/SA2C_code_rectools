3d5cc85074e6bbee68e2f83839e2262d41e95ccb
Anton Chernov <antonchernov@x5.ru>
2026-02-01 23:55:37 -0800

    Improve readme; ignore mlflow/

---
+++ scripts/publish_to_github.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -a; source .env; set +a
+
+: "${GITHUB_REPO_SSH:?Need GITHUB_REPO_SSH}"
+: "${PUBLIC_STRIP_PATH:=results/plots/persrec_tc5_2025-08-21/}"
+
+workdir="${1:-/tmp/repo-public-mirror}"
+
+rm -rf "$workdir"
+git clone --mirror "${CI_REPOSITORY_URL}" "$workdir"
+cd "$workdir"
+
+git remote add github "$GITHUB_REPO_SSH"
+
+# Remove secret/ from ALL commits
+git filter-repo --path "$PUBLIC_STRIP_PATH" --invert-paths --force
+
+# Push all refs
+git push --force --mirror github
